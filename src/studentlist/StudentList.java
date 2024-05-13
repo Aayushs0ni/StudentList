7d6627501a44ad0928893de9c8280f3e873103b0
AAYUSH SONI <AAYUSH SONI@10.48.76.84>
2024-05-13 13:59:02 -0400

    Initial commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author AAYUSH SONI
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Student[] studentList = new Student[3];
+        Student s1 = new Student();
+        s1.setName("Aayush Soni");
+        Student s2 = new Student();
+        s2.setName("Dev Patel");
+        Student s3 = new Student();
+        s3.setName("Nakul Chaudhry");
+        
+        studentList[0]=s1;
+        studentList[1]=s2;
+        studentList[2]=s3;
+        
+        for(Student s : studentList){
+        System.out.println(s.getName());}
+        
+    }
+}
